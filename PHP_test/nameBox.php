e0a38649a31749aed55f5a226575540f2c28a5a5
Juri Ando <jando16407c@gmail.com>
2019-06-04 00:23:43 -0400

    PHP example files

---
+++ PHP_test/nameBox.php
@@ -0,0 +1,17 @@
+<html>
+<head><title>input.html</title></head>
+<body>
+Type Name
+<form action="output.php" method="post">
+  <table border="1">
+    <tr>
+      <td>Name</td>
+      <td><input type="text" name="name"></td>
+      <td colspan="2" align="center">
+        <input type="submit" value="submit">
+      </td>
+    </tr>
+  </table>
+</form>
+</body>
+</html>

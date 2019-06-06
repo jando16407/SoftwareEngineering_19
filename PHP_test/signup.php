c4487a91483d191c03c68db9e4df18daca1bffe7
Juri Ando <jando16407c@gmail.com>
2019-06-05 21:50:45 -0400

    signup.php updated

--- PHP_test/signup.php
+++ PHP_test/signup.php
@@ -29,23 +29,27 @@
         <button>Sign Up</button>
     </p>
     <p>
-    <?php
-        $sectionNum = document.getElementById("secNum");
-        $sectionNumInput = document.getElementById("secNumInput");
-        $isAdmin = document.getElementById("isAdmin");
-      //  sectionNum.style.visibility = 'hidden';
-      //  $sectionNumInput.style.visibility = 'hidden';
-        $isAdmin.onclick = function(){
-            if(isAdmin.checked == false){
-            sectionNum.style.visibility = 'visible';
-            sectionNumInput.style.visibility = 'visible';
-            }
-            else{
+    
+        echo <<< EOM
+        <script>
+            var sectionNum = document.getElementById("secNum");
+            var sectionNumInput = document.getElementById("secNumInput");
+            var isAdmin = document.getElementById("isAdmin");
             sectionNum.style.visibility = 'hidden';
             sectionNumInput.style.visibility = 'hidden';
+            isAdmin.onclick = function(){
+                if(isAdmin.checked == false){
+                sectionNum.style.visibility = 'visible';
+                sectionNumInput.style.visibility = 'visible';
+                }
+                else{
+                sectionNum.style.visibility = 'hidden';
+                sectionNumInput.style.visibility = 'hidden';
+                }
             }
-        }
-    ?>
+        </script>
+    EOM;
+    
     </p>
 </body>
 </html>

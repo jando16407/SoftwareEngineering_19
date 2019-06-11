f957e7b49537c2215b77d24a8ea9fff9228f814c
Juri Ando <jando16407c@gmail.com>
2019-06-11 15:16:58 -0400

    Master branch merged

--- OI/login.js
+++ OI/login.js
@@ -1,6 +1,6 @@
-var firebase = require("firebase/app")
-require("firebase/auth")
-require("firebase/database")
+//var firebase = require("firebase")
+//require("firebase/auth")
+//require("firebase/database")
 
 var firebaseConfig = {
     apiKey: "AIzaSyB0ZY93KxJK4UIRVnyXWqNm2V1l1M-4j_4",
@@ -13,3 +13,62 @@ var firebaseConfig = {
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+  var database = firebase.database();
+  //reset login on window load
+  window.onload = function(){
+    firebase.auth().signOut().then(function(){
+      //successful
+//      alert('logged out')
+    },function(error){
+      alert('still logged in')
+    })
+  }
+  //did user hit login?
+  var login = document.getElementById("loginButton")
+
+  login.onclick = function(){
+        //get email & password from fields
+        var emailInput = document.getElementById("emailInput")
+        var passwordInput = document.getElementById("passwordInput")
+
+        var email = emailInput.value
+        var password = passwordInput.value
+       
+        // get email input & password input
+        firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            alert("error")
+            // ...
+        });
+    }
+  //checks if signed in and sends to appropriate page
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      var email = user.email;
+      //alert("LOGGED IN")
+      //sign in to appropriate page
+      identifyUser(email)
+    } else {
+      // User is signed out.
+      // ...
+    }
+  });
+
+function identifyUser(email){
+  var userId = email.match(/^(.+)@/)[1]
+  //save userId to send to next page
+  localStorage.setItem('user', userId)
+
+  //check db for admin or employee
+  var userType = firebase.database().ref('users/' + userId + '/userType').once('value').then(function(snapshot){
+    var type = snapshot.val() 
+    //send user to their page based on userType
+    if(type == 'Admin')
+      window.location.href = "User/Admin/main.html"
+    else
+      window.location.href = "User/main.html"
+  })
+  
+}

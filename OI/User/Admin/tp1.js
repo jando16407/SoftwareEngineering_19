fa43ca11d4ff00ed2e80d258eb5eeb21d9cc67c5
Juri Ando <jando16407c@gmail.com>
2019-06-19 17:32:49 -0400

    Detail View added

--- OI/User/Admin/tp1.js
+++ OI/User/Admin/tp1.js
@@ -16,6 +16,8 @@ var first1 = true;
 var first2 = true;
 var first3 = true;
 var first4 = true;
+var tableUnit1;
+var tableUnit2;
 
 
 /* */
@@ -46,7 +48,7 @@ function setup(){
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
     database = firebase.database();
-    console.log(firebase);
+//    console.log(firebase);
 }
 
 function got1(data) {
@@ -62,7 +64,7 @@ function got2(data) {
 function mainData1(data){
     var items = data.val();
     var keys = Object.keys(items);
-    console.log(keys);
+//    console.log(keys);
     if(first3){
         mainListContainer1 = document.createElement('table');
         document.getElementById("mainListUnit1").appendChild(mainListContainer1);
@@ -86,7 +88,7 @@ function mainData1(data){
             row.appendChild(id);
             row.appendChild(name);
             row.appendChild(desc);
-            console.log(row);
+//            console.log(row);
             mainListContainer1.appendChild(row);
         }
     first3 = false;
@@ -105,17 +107,17 @@ function mainData1(data){
             row.appendChild(id);
             row.appendChild(name);
             row.appendChild(desc);
-            console.log(row);
+//            console.log(row);
             mainListContainer1.appendChild(row);
     }
-    console.log(document.getElementsByName('itemTable3'));
+//    console.log(document.getElementsByName('itemTable3'));
 
 }
 
 function mainData2(data){
     var items = data.val();
     var keys = Object.keys(items);
-    console.log(keys);
+//    console.log(keys);
     if(first4){
         mainListContainer2 = document.createElement('table');
         document.getElementById("mainListUnit2").appendChild(mainListContainer2);
@@ -139,7 +141,7 @@ function mainData2(data){
             row.appendChild(id);
             row.appendChild(name);
             row.appendChild(desc);
-            console.log(row);
+//            console.log(row);
             mainListContainer2.appendChild(row);
         }
     first4 = false;
@@ -158,10 +160,10 @@ function mainData2(data){
             row.appendChild(id);
             row.appendChild(name);
             row.appendChild(desc);
-            console.log(row);
+//            console.log(row);
             mainListContainer2.appendChild(row);
     }
-    console.log(document.getElementsByName('itemTable4'));
+//    console.log(document.getElementsByName('itemTable4'));
 
 }
 
@@ -173,21 +175,23 @@ submitButton1.onclick = function(){
         itemName: document.getElementById("name1").value,
         itemDescription: document.getElementById("description1").value
     }
-    console.log(data);
+//    console.log(data);
     var result = ref1.push(data)
-    console.log(result.key);
+//    console.log(result.key);
 }
 
 function gotData1(data){
     //console.log(data.val());
     var items = data.val();
     var keys = Object.keys(items);
-    console.log(keys);
+//    console.log(keys);
     //console.log(document.getElementsByName('itemTable'));
     if(first1){
         listContainer1 = document.createElement('table');
         document.getElementById("officeItemList1").appendChild(listContainer1);
         listContainer1.setAttribute('name1', 'itemTable1');
+        listContainer1.setAttribute("id", "unit1Table");
+        tableUnit1 = document.getElementById("unit1Table");
         var listRow = document.createElement('tr');
         var topRow = "<th>ID</th><th>Item Name</th><th>Item Description</th>";
         listRow.innerHTML = topRow;
@@ -205,6 +209,13 @@ function gotData1(data){
 
         for( var i=0; i<keys.length; ++i ){
             var row = document.createElement('tr');
+            //var unit1row = "unit1row"+i;
+            //console.log(unit1row);
+            i += 100000;
+            row.setAttribute("onclick", "itemSelected("+i+")");
+            row.setAttribute("id", i);
+            console.log("Id = "+row.getAttribute("id"));
+            i -= 100000;
 
             var k = keys[i];
             var id = document.createElement('th');
@@ -223,7 +234,7 @@ function gotData1(data){
             row.appendChild(id);
             row.appendChild(name);
             row.appendChild(desc);
-            console.log(row);
+//            console.log(row);
             listContainer1.appendChild(row);
         }
     first1 = false;
@@ -242,10 +253,10 @@ function gotData1(data){
             row.appendChild(id);
             row.appendChild(name);
             row.appendChild(desc);
-            console.log(row);
+//            console.log(row);
             listContainer1.appendChild(row);
     }
-    console.log(document.getElementsByName('itemTable1'));
+//    console.log(document.getElementsByName('itemTable1'));
 
 }
 
@@ -254,22 +265,22 @@ function gotErr(err){
     console.log(err);
 }
 
-/******** Tab1 (Unit 2) ********************************/
+/******** Tab2 (Unit 2) ********************************/
 submitButton2.onclick = function(){
     var data = {
         itemId: document.getElementById("id2").value,
         itemName: document.getElementById("name2").value,
         itemDescription: document.getElementById("description2").value
     }
-    console.log(data);
+//    console.log(data);
     var result = ref2.push(data)
-    console.log(result.key);
+//    console.log(result.key);
 }
 
 function gotData2(data){
     var items = data.val();
     var keys = Object.keys(items);
-    console.log(keys);
+//    console.log(keys);
     if(first2){
         listContainer2 = document.createElement('table');
         document.getElementById("officeItemList2").appendChild(listContainer2);
@@ -293,7 +304,7 @@ function gotData2(data){
             row.appendChild(id);
             row.appendChild(name);
             row.appendChild(desc);
-            console.log(row);
+//            console.log(row);
             listContainer2.appendChild(row);
         }
     first2 = false;
@@ -312,10 +323,10 @@ function gotData2(data){
             row.appendChild(id);
             row.appendChild(name);
             row.appendChild(desc);
-            console.log(row);
+//            console.log(row);
             listContainer2.appendChild(row);
     }
-    console.log(document.getElementsByName('itemTable1'));
+//    console.log(document.getElementsByName('itemTable1'));
 
 }
 
@@ -324,5 +335,40 @@ function gotErr(err){
     console.log(err);
 }
 
+//tableUnit1 = document.getElementById("unit1Table");
+if( tableUnit1 != null ){
+    console.log("In the for loop");
+    for( var i=1; i<tableUnit1.rows.length; ++i ){
+        tableUnit1.onclick = function()
+        {
+            console.log("Clicked : "+this.cells[0].innerHTML);
+            //document.getElementById("detdetailViewId").value = this.cells[0].innerHTML;
+        }
+    }
+}
+
+function itemSelected(id){
+    var itemId = id;
+    if(100000<=id && id < 200000){
+        console.log(document.getElementById(itemId).cells[0]);
+        console.log(document.getElementById(itemId).cells[1]);
+        console.log(document.getElementById(itemId).cells[2]);
+        document.getElementById("detailViewId").innerHTML = document.getElementById(itemId).cells[0].innerHTML;
+        document.getElementById("detailViewName").innerHTML = document.getElementById(itemId).cells[1].innerHTML;
+        document.getElementById("detailViewDescription").innerHTML = document.getElementById(itemId).cells[2].innerHTML;
+
+        itemId -= 100000;
+        console.log("Clicked : "+itemId);
+        
+        
+    }
+    else if(200000<=id && id < 300000)
+        console.log("Clicked : "+id-200000);
+    else
+        console.log("not working");
+}
 
 //function displayList()
+
+
+//Display detail view table on left and right
--- OI/User/Admin/tp2.js
+++ OI/User/Admin/tp2.js
@@ -38,3 +38,21 @@ function openLink(evt, linkName) {
 }
 // Click on the first tablink on load
 document.getElementsByClassName("tablink")[0].click();
+
+/* For the tabs on bottom */
+// Tabs
+function secondopenLink(evt, linkName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("secondmyLink");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("secondtablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+  }
+  document.getElementById(linkName).style.display = "block";
+  evt.currentTarget.className += " w3-red";
+}
+// Click on the first tablink on load
+document.getElementsByClassName("secondtablink")[0].click();

6ce8ca85dae8f5fd7de23bb3a7792d8b2e83425b
Juri Ando <jando16407c@gmail.com>
2019-06-27 01:11:13 -0400

    Admin Announcement Completed

--- OI/User/Admin/main.js
+++ OI/User/Admin/main.js
@@ -1,11 +1,14 @@
 var database;
 var announcementsPath, ref;
-var submitButton1;
+var submitButton1, deleteButton;
+var announcementContainer;
+var currentDate = new Date();
+var childNodePath;
+var selecedItem;
 
 
 firebase_setup();
 page_setup();
-init_list();
 renderListen();
 
 
@@ -30,22 +33,11 @@ function firebase_setup(){
 //initial setup for the page
 function page_setup(){
     submitButton1 = document.getElementById("submitButton1");
+    deleteButton = document.getElementById("deleteButton");
     announcementsPath = "Announcements"
     ref = database.ref(announcementsPath);
-}
-
-//Initial rendering of tables
-function init_list(){ 
-    //Main Tab setup
-    masterListContainer = document.createElement('table');
-    document.getElementById("masterItemList").appendChild(masterListContainer);
-    masterListContainer.setAttribute('name0', 'itemTable0');
-    masterListContainer.setAttribute("id", "masterTable");
-    tableMaster = document.getElementById("masterTable");
-    var listRow_0 = document.createElement('tr');
-    var topRow_0 = "<th>Unit</th><th>ID</th><th>Item Name</th><th>Item Description</th>";
-    listRow_0.innerHTML = topRow_0;
-    masterListContainer.appendChild(listRow_0);
+    announcementContainer = document.getElementById("pastAnnouncementsList");
+    selecedItem = document.getElementById("selectedAnnouncement");
 }
 
 //Error handling
@@ -56,130 +48,164 @@ function gotErr(err){
 
 /* Initialize Functions End */
 
+
+
 /* Rendering funcitons Start */
 
 //Render table, clear the table everytime it's called and display new data
 function renderTableContents(path){
-    //Rendering for MasterList
-    if(path == masterPath){
-        while(masterListContainer.children.length > 1){
-            masterListContainer.removeChild(masterListContainer.childNodes[1]);
+    //Rendering for Annnouncements
+    if(path == announcementsPath){
+        while(announcementContainer.children.length > 0){
+            announcementContainer.removeChild(announcementContainer.childNodes[1]);
         }
-        renderMasterList();
-    }
-    //Rendering for Unit 1
-    else if(path == unitPath1){
-        while(listContainer1.children.length > 1){
-            listContainer1.removeChild(listContainer1.childNodes[1]);
-        }
-        renderUnit1();
-    }
-    //Rendering for Unit 2
-    else  if(path == unitPath2){
-        while(listContainer2.children.length > 1){
-            listContainer2.removeChild(listContainer2.childNodes[1]);
-        }
-        renderUnit2();
+        renderAnnouncements();
     }
 }
 
-//Update the table for masterList
-function renderMasterList(){
-    refAdmin.once("value", function(snapshot){
+//Update the table for Annnouncements
+function renderAnnouncements(){
+    ref.once("value", function(snapshot){
         let items = snapshot.val();
         let keys = Object.keys(items);
-        for( let i=0; i<keys.length; ++i ){
-            let row = document.createElement('tr');
+        for( let i=keys.length-1; i>=0; --i ){
             let k = keys[i];
-            let childNodeId = masterListContainer.children.length;
-            let deletePath = items[k].UnitPath+"/"+items[k].ItemKey;
-            row.addEventListener("click", function(){
-                itemSelected(childNodeId, deletePath);
+            let announcement = document.createElement('div');
+            let title = document.createElement('div');
+            let date = document.createElement('p');
+            let body = document.createElement('p');
+            let id = announcementContainer.children.length + 1;
+            announcement.setAttribute("key", keys[i]);
+            announcement.setAttribute("class", 'ui message');
+            announcement.addEventListener("click", function(){
+                itemSelected(id, announcement);
             });
-            row.setAttribute("key", keys[i]);
-            let unit = document.createElement('th');
-            let id = document.createElement('th');
-            let name = document.createElement('th');
-            let desc = document.createElement('th');
-            unit.innerHTML = items[k].UnitNum;
-            id.innerHTML = items[k].itemId;
-            name.innerHTML = items[k].itemName;
-            desc.innerHTML = items[k].itemDescription;
-            row.appendChild(unit);
-            row.appendChild(id);
-            row.appendChild(name);
-            row.appendChild(desc);
-            masterListContainer.appendChild(row);
+            title.setAttribute("class", 'header');
+            title.innerHTML = items[k].title;
+            body.innerHTML = items[k].body;
+            date.innerHTML = items[k].date;
+            announcement.appendChild(title);
+            announcement.appendChild(date);
+            announcement.appendChild(body);
+            announcementContainer.appendChild(announcement);
         }
     }, gotErr);
 }
 
 /* Rendering funcitons End */
 
+
+
 /* onClick handlings Start */
+
+//Handle when item in a list is clicked
+function itemSelected(id, container){
+    childNodePath = document.getElementById("pastAnnouncementsList").childNodes[id].getAttribute("key");
+    selecedItem.value = document.getElementById("pastAnnouncementsList").childNodes[id].childNodes[0].innerHTML;
+    console.log(selecedItem);
+    console.log(id);
+}
 
 //Submit button handling, push data to firebase
 submitButton1.onclick = function(){
     //Push data to Unit
     let data = {
-        itemId: document.getElementById("id1").value,
-        itemName: document.getElementById("name1").value,
-        itemDescription: document.getElementById("description1").value,
+        title: document.getElementById("title").value,
+        body: document.getElementById("body").value.replace(/\r?\n/g, '<br />'),
+        date: currentDate.getMonth()+'/'+currentDate.getDate()+'/'+currentDate.getFullYear()
     }
-    ref1.push(data)
-    //Push data to MasterList
-    let masterData = {
-        itemId: document.getElementById("id1").value,
-        itemName: document.getElementById("name1").value,
-        itemDescription: document.getElementById("description1").value,
-        UnitNum : "001",
-        UnitPath : unitPath1,
-        ItemKey : newItemKey
-    }
-    refAdmin.push(masterData);
-    //update unit with master key
-    database.ref(unitPath1+"/"+newItemKey).set({
-        itemId: document.getElementById("id1").value,
-        itemName: document.getElementById("name1").value,
-        itemDescription: document.getElementById("description1").value,
-        masterKey : newItemKey2
-    });
+    ref.push(data)
+    document.getElementById("title").value = "";
+    document.getElementById("body").value = "";
 }
 
+//Delete button handling
+deleteButton.onclick = function(){
+    //Check if item is selected or not
+    let deleteRef = database.ref(announcementsPath+'/'+childNodePath);
+    deleteRef.remove();
+    childNodePath = "";
+    selecedItem.value = "";
+}
+
+
 /* onClick handlings End */
+
+
+
 
 /* Database modify handling start */
 
 //Listen to any value changes on the database
 function renderListen(){
     //Any item modification, adding, deleting will update the 
-    //list in MasterList
-    refAdmin.on("value", function(snapshot){
-        renderTableContents(masterPath);
-    }, gotErr);
-    //Any item modification, adding, deleting will update the 
-    //list in unit 1
-    ref1.on("value", function(snapshot){
-        renderTableContents(unitPath1);
-    }, gotErr);
-    //Any item modification, adding, deleting will update the 
-    //list in unit 2
-    ref2.on("value", function(snapshot){
-        renderTableContents(unitPath2);
-    }, gotErr);
-    //when item is added to masterlist
-    refAdmin.on("child_added", function(snapshot){
-        newItemKey2 = snapshot.key;
-    }, gotErr);
-    //when item is added to unit 1
-    ref1.on("child_added", function(snapshot){
-        newItemKey = snapshot.key;
-    }, gotErr);
-    //when item is added to unit 2
-    ref2.on("child_added", function(snapshot){
-        newItemKey = snapshot.key;
+    //list in Announcement
+    ref.on("value", function(snapshot){
+        renderTableContents(announcementsPath);
     }, gotErr);
 }
 
 /* Database modify handling end */
 
+
+
+/* Basic Page JavaScript Start */
+
+// Get the Sidebar
+var mySidebar = document.getElementById("mySidebar");
+
+// Get the DIV with overlay effect
+var overlayBg = document.getElementById("myOverlay");
+
+// Toggle between showing and hiding the sidebar, and add overlay effect
+function w3_open() {
+  if (mySidebar.style.display === 'block') {
+    mySidebar.style.display = 'none';
+    overlayBg.style.display = "none";
+  } else {
+    mySidebar.style.display = 'block';
+    overlayBg.style.display = "block";
+  }
+}
+
+// Close the sidebar with the close button
+function w3_close() {
+  mySidebar.style.display = "none";
+  overlayBg.style.display = "none";
+}
+
+// Tabs
+function openLink(evt, linkName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("myLink");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+  }
+  document.getElementById(linkName).style.display = "block";
+  evt.currentTarget.className += " w3-red";
+}
+
+// Tabs
+function openLink2(evt, linkName) {
+  var i, x, tttabbb;
+  x = document.getElementsByClassName("myLink2");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tttabbb = document.getElementsByClassName("tttabbb");
+  for (i = 0; i < x.length; i++) {
+    tttabbb[i].className = tttabbb[i].className.replace(" w3-red", "");
+  }
+  document.getElementById(linkName).style.display = "block";
+  evt.currentTarget.className += " w3-red";
+}
+
+
+// Click on the first tablink on load
+document.getElementsByClassName("tablink")[0].click();
+document.getElementsByClassName("tttabbb")[0].click();
+/* Basic Page JavaScript End */

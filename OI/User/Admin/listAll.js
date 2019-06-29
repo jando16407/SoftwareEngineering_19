b62f8d3e524f5c4468133a5060601089a5e57cbf
watikarosm <45465335+watikarosm@users.noreply.github.com>
2019-06-29 18:36:39 -0400

    Install firebase 6.2.4

--- OI/User/Admin/listAll.js
+++ OI/User/Admin/listAll.js
@@ -1,5 +1,27 @@
-db.collection('Watis').get().then((snapshot)=>{
-    snapshot.docs.forEach(doc =>{
-        console.log(doc)
+const firebaseConfig = {
+    apiKey: "AIzaSyB0ZY93KxJK4UIRVnyXWqNm2V1l1M-4j_4",
+    authDomain: "office-inventory-12f99.firebaseapp.com",
+    databaseURL: "https://office-inventory-12f99.firebaseio.com",
+    projectId: "office-inventory-12f99",
+    storageBucket: "office-inventory-12f99.appspot.com",
+    messagingSenderId: "147848186588",
+    appId: "1:147848186588:web:33dbc8d727af1de4"
+  };
+firebase.initializeApp(firebaseConfig);
+let firestore = firebase.firestore();
+const dbRef = firestore.doc("Watis/NusiCkayiV6LuuMOu94U/Inventory");
+const printOut = document.querySelector('#supplyList');
+const showAll = document.querySelector("#showAll");
+const sndBtn = document.querySelector("sndBtn");
+
+sndBtn.addEventListener("click", function(){
+    const textToSnd = showAll.value;
+    console.log("I am showing it... " + textToSnd + "to firestore.");
+    dbRef.set({
+        printOut: textToSnd
+    }).then(function(){
+        alert("Request sent...");
+    }).catch(function (e){
+        alert("Request not sent...");
     })
 })

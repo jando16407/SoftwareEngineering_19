d6d139c1205bf6a5a35dae2e1768ffd84075f53e
Juri Ando <jando16407c@gmail.com>
2019-07-02 14:12:36 -0400

    Shortcut upated

--- OI/User/Admin/shortcuts.js
+++ OI/User/Admin/shortcuts.js
@@ -2,7 +2,6 @@ hotkeys('ctrl+1,ctrl+2,ctrl+3,ctrl+4,ctrl+5', function(event,handler) {
   switch(handler.key){
     case "ctrl+1": window.location.href ="main.html";break;
     case "ctrl+2": window.location.href ="workspace.html";break;
-    //case "ctrl+m": window.location.href ="messages.html";break;
     case "ctrl+3":window.location.href ="settings.html";break;
     case "ctrl+4":window.location.href ="officeview.html";break;
     case "ctrl+5":window.location.href ="../../logout.html";break;

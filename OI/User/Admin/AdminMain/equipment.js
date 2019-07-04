f324211afe2ea9c3d6ef868a3df3027d5e86b821
watikarosm <45465335+watikarosm@users.noreply.github.com>
2019-07-04 19:51:50 -0400

    Formatted Admin Main Panel

--- OI/User/Admin/AdminMain/equipment.js
+++ OI/User/Admin/AdminMain/equipment.js
@@ -1,17 +1,4 @@
 
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyB0ZY93KxJK4UIRVnyXWqNm2V1l1M-4j_4",
-    authDomain: "office-inventory-12f99.firebaseapp.com",
-    databaseURL: "https://office-inventory-12f99.firebaseio.com",
-    projectId: "office-inventory-12f99",
-    storageBucket: "office-inventory-12f99.appspot.com",
-    messagingSenderId: "147848186588",
-    appId: "1:147848186588:web:33dbc8d727af1de4"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  const db = firebase.firestore();
   // create element and render item list
 function renderList(doc){
     let li = document.createElement('li');
@@ -20,8 +7,7 @@ function renderList(doc){
     let condition = document.createElement('span');
     let col1 = document.createElement('span');
     let col2 = document.createElement('span');
-
-
+    
     li.setAttribute('data-id', doc.id);
     itemName.textContent = doc.data().itemName;
     itemDescription.textContent = doc.data().itemDescription;
@@ -38,7 +24,8 @@ function renderList(doc){
     printOut.appendChild(li);
 }
 
-db.collection('Office/Inventory/Units').orderBy('itemName').onSnapshot((snapshot) =>{
+db.collection('Watis/NusiCkayiV6LuuMOu94U/Inventory').orderBy('itemName').onSnapshot((snapshot) =>{
+
   let changes = snapshot.docChanges();
   changes.forEach(change =>{
       if(change.type == 'added'){

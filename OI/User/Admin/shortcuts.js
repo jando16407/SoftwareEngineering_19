69e801742fbecd917da5a56e0a606421f5ba22e3
Amir Seifpour <aseifpour@yahoo.com>
2019-07-05 13:47:11 -0400

    shortcut

--- OI/User/Admin/shortcuts.js
+++ OI/User/Admin/shortcuts.js
@@ -1,10 +1,9 @@
-hotkeys('ctrl+p,ctrl+w,ctrl+m,ctrl+s,ctrl+o,ctrl+l', function(event,handler) {
+hotkeys('shift+ctrl+1,shift+ctrl+2,shift+ctrl+3,shift+ctrl+4,shift+ctrl+5', function(event,handler) {
   switch(handler.key){
-    case "ctrl+p": window.location.href ="main.html";break;
-    case "ctrl+w": window.location.href ="workspace.html";break;
-    case "ctrl+m": window.location.href ="messages.html";break;
-    case "ctrl+s":window.location.href ="settings.html";break;
-    case "ctrl+o":window.location.href ="officeview.html";break;
-    case "ctrl+l":window.location.href ="../../logout.html";break;
+    case "shift+ctrl+1": window.location.href ="main.html";break;
+    case "shift+ctrl+2": window.location.href ="workspace.html";break;
+    case "shift+ctrl+3":window.location.href ="settings.html";break;
+    case "shift+ctrl+4":window.location.href ="officeview.html";break;
+    case "shift+ctrl+5":window.location.href ="../../logout.html";break;
   }
 });
--- OI/User/shortcuts.js
+++ OI/User/shortcuts.js
@@ -1,10 +1,8 @@
-hotkeys('shift+ctrl+p,ctrl+w,ctrl+m,ctrl+s,ctrl+o,ctrl+l', function(event,handler) {
+hotkeys('shift+ctrl+1,shift+ctrl+2,shift+ctrl+3,shift+ctrl+4', function(event,handler) {
   switch(handler.key){
-    case "shift+ctrl+p": window.location.href ="main.html";break;
-    case "ctrl+w": window.location.href ="user_workspace.html";break;
-    //case "ctrl+m": window.location.href ="messages.html";break;
-    case "ctrl+s":window.location.href ="settings.html";break;
-    //case "ctrl+o":window.location.href ="officeview.html";break;
-    case "ctrl+l":window.location.href ="../logout.html";break;
+    case "shift+ctrl+1": window.location.href ="main.html";break;
+    case "shift+ctrl+2": window.location.href ="user_workspace.html";break;
+    case "shift+ctrl+3":window.location.href ="settings.html";break;
+    case "shift+ctrl+4":window.location.href ="../logout.html";break;
   }
 });

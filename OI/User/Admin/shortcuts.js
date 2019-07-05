137bd3b56c348c6adb8b13aeb4cc9e01d993769b
Amir Seifpour <aseifpour@yahoo.com>
2019-07-05 15:38:01 -0400

    new update setting

--- OI/User/Admin/shortcuts.js
+++ OI/User/Admin/shortcuts.js
@@ -1,9 +1,9 @@
-hotkeys('ctrl+1,ctrl+2,ctrl+3,ctrl+4,ctrl+5', function(event,handler) {
+hotkeys('shift+ctrl+1,shift+ctrl+2,shift+ctrl+3,shift+ctrl+4,shift+ctrl+5', function(event,handler) {
   switch(handler.key){
-    case "ctrl+1": window.location.href ="main.html";break;
-    case "ctrl+2": window.location.href ="workspace.html";break;
-    case "ctrl+3":window.location.href ="settings.html";break;
-    case "ctrl+4":window.location.href ="officeview.html";break;
-    case "ctrl+5":window.location.href ="../../logout.html";break;
+    case "shift+ctrl+1": window.location.href ="main.html";break;
+    case "shift+ctrl+2": window.location.href ="workspace.html";break;
+    case "shift+ctrl+3":window.location.href ="settings.html";break;
+    case "shift+ctrl+4":window.location.href ="officeview.html";break;
+    case "shift+ctrl+5":window.location.href ="../../logout.html";break;
   }
 });
